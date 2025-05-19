2a26beaf5288804a6c74b36d0ee13bfdd1e95a80
Pranav Rathi <99244980+pvrPranavRathi@users.noreply.github.com>
2025-05-19 16:56:32 +0530

    Update material.module.ts

--- dummy-app-FrontEnd/src/app/material/material.module.ts
+++ dummy-app-FrontEnd/src/app/material/material.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button'
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
+// Array to import all modules from material library
 const MaterialComponents = [
-  MatButtonModule,
-  MatSlideToggleModule,
   MatTableModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatSelectModule,
-  MatSortModule,
   MatPaginatorModule
 ]
 
